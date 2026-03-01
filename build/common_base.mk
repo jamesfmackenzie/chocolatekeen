b499dc1abb60bfa346aa1df4d695b10cfc97fc0b
James Mackenzie <jim@lilandjim.com>
2026-03-01 11:43:24 -0500

    Move shared base make variables into common fragment

---
+++ build/common_base.mk
@@ -0,0 +1,16 @@
+DEBUG ?= 0
+BUILDASCPP ?= 0
+BINPREFIX ?=
+ifeq ($(origin CC), default)
+CC := gcc
+endif
+
+STRIPBIN ?= $(BINPREFIX)strip
+WINDRES ?= windres
+SDLCONFIG ?= sdl2-config
+SRC ?= ../../src
+RSRC ?= ../../rsrc
+OBJ ?= obj
+
+USE_OPENGL ?= 1
+SINGLE_EPISODE ?= 0

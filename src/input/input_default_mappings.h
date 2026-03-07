77365c53abad29e06d95247aa8bb4253c58e38cb
James Mackenzie <jim@lilandjim.com>
2026-03-06 23:39:33 -0500

    input: extract default mappings into dedicated module
    Move large default input mapping data/constants out of input.c into input_default_mappings.c/.h, update input.c to consume exported mappings, and wire the new module into shared makefiles, tests, and Visual Studio project.

---
+++ src/input/input_default_mappings.h
@@ -0,0 +1,17 @@
+#ifndef _CHOCOLATE_KEEN_INPUT_DEFAULT_MAPPINGS_
+#define _CHOCOLATE_KEEN_INPUT_DEFAULT_MAPPINGS_
+
+#include "input/input.h"
+
+extern const MappedInputEvent_T defaultKeyMappings[];
+extern const int defaultKeyMappingsCount;
+extern const MappedInputEvent_T defaultJoyButtonMappings[];
+extern const MappedInputEvent_T defaultJoyPAxisMappings[];
+extern const MappedInputEvent_T defaultJoyNAxisMappings[];
+extern const MappedInputEvent_T defaultFullScreenHandlerMapping;
+extern const MappedInputEvent_T defaultCursorLockHandlerMapping;
+extern const MappedInputEvent_T defaultQuitHandlerMapping;
+extern const MappedInputEvent_T defaultFirstModifierMapping;
+extern const MappedInputEvent_T defaultSecondModifierMapping;
+
+#endif

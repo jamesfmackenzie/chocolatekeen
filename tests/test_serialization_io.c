4777322ce08e8192a00aeb9cfffc874e2fe38543
James Mackenzie <jim@lilandjim.com>
2026-03-06 22:15:24 -0500

    Expand serialization IO failure propagation tests

--- tests/test_serialization_io.c
+++ tests/test_serialization_io.c
@@ -16,6 +16,33 @@ typedef struct {
     uint8_t c;
 } TestPayload_T;
 
+typedef struct {
+    size_t returnValues[3];
+    int nextIndex;
+    int evalCount;
+} TestIoSequence_T;
+
+static size_t test_io_sequence_next(TestIoSequence_T *seq) {
+    seq->evalCount++;
+    return seq->returnValues[seq->nextIndex++];
+}
+
+static bool read_payload_with_sequence(TestIoSequence_T *seq) {
+    bool loadOk = true;
+    CK_IO_EXPECT(loadOk, test_io_sequence_next(seq), 1);
+    CK_IO_EXPECT(loadOk, test_io_sequence_next(seq), 1);
+    CK_IO_EXPECT(loadOk, test_io_sequence_next(seq), 1);
+    return loadOk;
+}
+
+static bool write_payload_with_sequence(TestIoSequence_T *seq) {
+    bool saveOk = true;
+    CK_IO_EXPECT(saveOk, test_io_sequence_next(seq), 1);
+    CK_IO_EXPECT(saveOk, test_io_sequence_next(seq), 1);
+    CK_IO_EXPECT(saveOk, test_io_sequence_next(seq), 1);
+    return saveOk;
+}
+
 static bool read_payload(FILE *fp, TestPayload_T *out) {
     bool loadOk = true;
     CK_IO_EXPECT(loadOk, fread(out->a, sizeof(out->a), 1, fp), 1);
@@ -76,6 +103,25 @@ TEST_SUITE(test_serialization_read_short_middle_fails)
     fclose(fp);
 }
 
+TEST_SUITE(test_serialization_read_short_last_fails)
+{
+    TestPayload_T actual = {0};
+    uint8_t partial_a[4] = {1, 2, 3, 4};
+    uint8_t partial_b[2] = {5, 6};
+
+    FILE *fp = tmpfile();
+    TEST_ASSERT(fp != NULL);
+    if (!fp)
+        return;
+
+    write_bytes_or_fail(fp, partial_a, sizeof(partial_a));
+    write_bytes_or_fail(fp, partial_b, sizeof(partial_b));
+    rewind(fp);
+
+    TEST_ASSERT(!read_payload(fp, &actual));
+    fclose(fp);
+}
+
 TEST_SUITE(test_serialization_write_success)
 {
     TestPayload_T expected = {{10, 20, 30, 40}, {50, 60}, 70};
@@ -116,10 +162,43 @@ TEST_SUITE(test_serialization_write_to_readonly_fails)
     remove(tmpPath);
 }
 
+TEST_SUITE(test_serialization_read_failure_short_circuits_following_steps)
+{
+    TestIoSequence_T seq = {{1, 0, 1}, 0, 0};
+    TEST_ASSERT(!read_payload_with_sequence(&seq));
+    TEST_ASSERT_EQUAL(2, seq.evalCount);
+}
+
+TEST_SUITE(test_serialization_write_failure_short_circuits_following_steps)
+{
+    TestIoSequence_T seq = {{1, 0, 1}, 0, 0};
+    TEST_ASSERT(!write_payload_with_sequence(&seq));
+    TEST_ASSERT_EQUAL(2, seq.evalCount);
+}
+
+TEST_SUITE(test_serialization_read_failure_on_first_step_short_circuits_all_remaining)
+{
+    TestIoSequence_T seq = {{0, 1, 1}, 0, 0};
+    TEST_ASSERT(!read_payload_with_sequence(&seq));
+    TEST_ASSERT_EQUAL(1, seq.evalCount);
+}
+
+TEST_SUITE(test_serialization_write_failure_on_last_step_reports_failure)
+{
+    TestIoSequence_T seq = {{1, 1, 0}, 0, 0};
+    TEST_ASSERT(!write_payload_with_sequence(&seq));
+    TEST_ASSERT_EQUAL(3, seq.evalCount);
+}
+
 TEST_SUITE(test_serialization_io)
 {
     RUN_TEST(test_serialization_read_success);
     RUN_TEST(test_serialization_read_short_middle_fails);
+    RUN_TEST(test_serialization_read_short_last_fails);
     RUN_TEST(test_serialization_write_success);
     RUN_TEST(test_serialization_write_to_readonly_fails);
+    RUN_TEST(test_serialization_read_failure_short_circuits_following_steps);
+    RUN_TEST(test_serialization_write_failure_short_circuits_following_steps);
+    RUN_TEST(test_serialization_read_failure_on_first_step_short_circuits_all_remaining);
+    RUN_TEST(test_serialization_write_failure_on_last_step_reports_failure);
 }
